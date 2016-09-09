43a708a2871f7602efb4427dac8c80a9ebb12b84
Cassio Kawakita <cassio.kawakita@gmail.com>
2016-09-09 13:22:18 -0300

    Added production host

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => ENV['MANDRILL_ADDRESS'],
+  :port           => ENV['MANDRILL_PORT'],
+  :authentication => ENV['MANDRILL_AUTHENTICATION'],
+  :user_name      => ENV['MANDRILL_USERNAME'],
+  :password       => ENV['MANDRILL_PASSWORD'],
+  :domain         => ENV['MANDRILL_DOMAIN'],
+  :enable_starttls_auto => true
+}
